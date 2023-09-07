7259175f86a738c2af08c6df51cd8c387a8137aa
TomaszM-dev <tomekmalocha@gmail.com>
2023-09-07 17:11:52 +0200

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Button variant={"link"}>Click me</Button>
+      </div>
+    </div>
+  );
+}
