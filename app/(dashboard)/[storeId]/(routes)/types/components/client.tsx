3d171fa3118e77a208129d063296a8316e1de042
TomaszM-dev <tomekmalocha@gmail.com>
2023-10-03 01:30:50 +0200

    fixes

--- app/(dashboard)/[storeId]/(routes)/types/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/types/components/client.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Billboard } from "@prisma/client";
+
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { TypeColumn, columns } from "./columns";
